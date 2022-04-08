f5c7be44b82820a4565609542ce24ae00384c29d
Alex Telegin <telegin.alexander@gmail.com>
2022-04-08 23:30:33 +0400

    Add price proxy interface

---
+++ contracts/OGS/OGSPriceProxy.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.9;
+pragma experimental ABIEncoderV2;
+
+import { IEACAggregatorProxy } from "./intf/IEACAggregatorProxy.sol";
+import { IOGSPriceProxy } from "./intf/IOGSPriceProxy.sol";
+
+contract OGSPriceProxy is IOGSPriceProxy {
+
+    IEACAggregatorProxy private _refDON;
+
+    constructor(address refDON) public {
+        _refDON = IEACAggregatorProxy(refDON);
+    }
+
+    function latestPrice() external override view returns (uint128) {
+        return 0;
+    }
+
+    function decimals() external override view returns (uint8) {
+        return 0;
+    }
+}
